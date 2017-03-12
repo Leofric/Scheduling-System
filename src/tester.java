2aaf4ba0d3e90e0c08a2347b965885acc8814e80
Alex Berthon <Alexjberthon@hotmail.com>
2017-03-12 16:23:57 -0700

    Implemented change username, change password, and display schedule options, also fixed error message display

--- src/tester.java
+++ src/tester.java
@@ -1,26 +1,38 @@
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class tester {
 
 	public static void main(String[] args) {
-		try{
-			//jdbc:mysql://localhost:3306/Peoples?autoReconnect=true&useSSL=false
-			Connection myconn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?autoReconnect=false&useSSL=false", "root", "punkulam");
-			Statement myStm = myconn.createStatement();
-			myStm.executeUpdate("INSERT INTO useraccount VALUES('1', '1', '2', '2', '2')");
-			ResultSet myRs = myStm.executeQuery("select userpassword from useraccount");
-			while(myRs.next()){
-				System.out.println(myRs.getString("userpassword"));
-			}
-			myconn.close();
-			myStm.close();
-			myRs.close();
-			
-			//myStm.
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date));
+		
+		
+		
+		
+		
+//		try{
+//			//jdbc:mysql://localhost:3306/Peoples?autoReconnect=true&useSSL=false
+//			Connection myconn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?autoReconnect=false&useSSL=false", "root", "punkulam");
+//			Statement myStm = myconn.createStatement();
+//			myStm.executeUpdate("INSERT INTO useraccount VALUES('1', '1', '2', '2', '2')");
+//			ResultSet myRs = myStm.executeQuery("select userpassword from useraccount");
+//			while(myRs.next()){
+//				System.out.println(myRs.getString("userpassword"));
+//			}
+//			myconn.close();
+//			myStm.close();
+//			myRs.close();
+//			
+//			//myStm.
+//		}
+//		catch(Exception e){
+//			e.printStackTrace();
+//		}
 	}
 }
 
